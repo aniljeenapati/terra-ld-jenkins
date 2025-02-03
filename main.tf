d7b8c1877ec3d154bdeb5ab478b7d4ae33ab6dcd
aniljeenapati <jeenapati.anil@gmail.com>
2025-02-03 11:52:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
 module "http_lb" {
   source = "../../modules/http-lb"
 
-  backend_service_name  = "dev-backend-service"
-  instance_group        = "projects/anil-449609/zones/us-central1-a/instanceGroups/dev-instance-group"
-  health_check_name     = "dev-health-check"
-  url_map_name          = "dev-url-map"
-  target_proxy_name     = "dev-target-proxy"
-  forwarding_rule_name  = "dev-forwarding-rule"
+  machine_type         = "e2-medium"
+  source_image         = "debian-cloud/debian-10"
+  network              = "default"
+  mig_name             = "dev-mig"
+  zone                 = "us-central1-a"
+  target_size          = 2
+  backend_service_name = "dev-backend-service"
+  health_check_name    = "dev-health-check"
+  url_map_name         = "dev-url-map"
+  target_proxy_name    = "dev-target-proxy"
+  forwarding_rule_name = "dev-forwarding-rule"
 }
