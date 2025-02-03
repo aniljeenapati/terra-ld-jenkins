827e04eb94c3227d20a22982544724c28098d599
aniljeenapati <jeenapati.anil@gmail.com>
2025-02-03 11:47:56 +0530

    Update main.tf

--- modules/http-lb/main.tf
+++ modules/http-lb/main.tf
@@ -1,3 +1,55 @@
+# Instance Template
+resource "google_compute_instance_template" "default" {
+  name_prefix = "instance-template-"
+  description = "Instance template for HTTP Load Balancer backend"
+
+  machine_type = var.machine_type
+  tags         = ["http-server"]
+
+  disk {
+    source_image = var.source_image
+    auto_delete  = true
+    boot         = true
+  }
+
+  network_interface {
+    network = var.network
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y apache2
+    echo "<h1>Hello from $(hostname)</h1>" > /var/www/html/index.html
+  EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Managed Instance Group (MIG)
+resource "google_compute_instance_group_manager" "default" {
+  name               = var.mig_name
+  base_instance_name = "http-lb-instance"
+  zone               = var.zone
+
+  version {
+    instance_template = google_compute_instance_template.default.self_link
+  }
+
+  target_size = var.target_size
+
+  auto_healing_policies {
+    health_check      = google_compute_health_check.default.self_link
+    initial_delay_sec = 300
+  }
+}
+
+# Backend Service
 resource "google_compute_backend_service" "default" {
   name        = var.backend_service_name
   port_name   = "http"
@@ -5,12 +57,13 @@ resource "google_compute_backend_service" "default" {
   timeout_sec = 10
 
   backend {
-    group = var.instance_group
+    group = google_compute_instance_group_manager.default.instance_group
   }
 
   health_checks = [google_compute_health_check.default.self_link]
 }
 
+# Health Check
 resource "google_compute_health_check" "default" {
   name = var.health_check_name
 
@@ -19,16 +72,19 @@ resource "google_compute_health_check" "default" {
   }
 }
 
+# URL Map
 resource "google_compute_url_map" "default" {
   name            = var.url_map_name
   default_service = google_compute_backend_service.default.self_link
 }
 
+# Target HTTP Proxy
 resource "google_compute_target_http_proxy" "default" {
   name    = var.target_proxy_name
   url_map = google_compute_url_map.default.self_link
 }
 
+# Global Forwarding Rule
 resource "google_compute_global_forwarding_rule" "default" {
   name       = var.forwarding_rule_name
   target     = google_compute_target_http_proxy.default.self_link
